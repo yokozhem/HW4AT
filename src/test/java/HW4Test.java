906742a8cabe659cd83762465e438cbd5d5abd40
yokozhem <kozhemyakova.elena@gmail.com>
2021-12-14 21:39:57 +0700

    HW4 homework update

--- src/test/java/HW4Test.java
+++ src/test/java/HW4Test.java
@@ -1,13 +1,22 @@
 import org.junit.jupiter.api.*;
-import org.junit.platform.commons.logging.LoggerFactory;
-
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class HW4Test {
+    private static Logger logger = LoggerFactory.getLogger(HW4Test.class);
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Метод выполнится перед всеми тестами 1 раз ");
+    }
+
     @Test
      @DisplayName("Позитивная проверка площади треугольника")
-    void givenTreeSidesWhenAreaTriangleThenPositive(){
+       void givenTreeSidesWhenAreaTriangleThenPositive(){
+        logger.trace("trace log");
+        logger.info("info log");
+        logger.debug("debug log");
+
         Double result = HW4.areaTriangle(3,4,5);
         Assertions.assertEquals(result, 6);
     }
@@ -15,12 +24,13 @@ public class HW4Test {
     @Test
     @DisplayName("Негативная проверка на невалидные значения сторон")
  void givenThreeSidesIsNotValidWhenAreaTriangleThenNegative(){
+        logger.trace("trace log");
+        logger.info("info log");
+        logger.debug("debug log");
+
         Double result = HW4.areaTriangle(30,40, 5);
         Assertions.assertEquals(result, null);
     }
-
-
-    //public static Logger logger = (Logger) LoggerFactory.getLogger(HW4Test.class);
 }
 
 
